1df211f7061cfddd23b11c82f5d796ddc1a08298
T.J <t.ruggles@live.ca>
2019-05-09 21:28:35 -0400

    Adding products component

---
+++ src/components/Products.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Products = () => (
+    <div>
+        <div>Purses & clutches</div>
+        <div>Bags</div>
+        <div>Card and coin holders</div>
+        <div>Sleeves</div>
+        <div>Christmas stockings</div>
+    </div>
+)
+
+export default Products
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import React from "react"
 import Layout from "../components/Layout"
 import Header from "../components/Header"
 import HeroBanner from "../components/HeroBanner"
+import Products from "../components/Products"
 
 const IndexPage = () => (
     <Layout>
         <Header />
         <HeroBanner />
+        <Products />
     </Layout>
 )
 
