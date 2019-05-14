a0c5737acf58a4712e66f43aeba5898b7439f2b3
T.J <t.ruggles@live.ca>
2019-05-14 17:13:04 -0400

    Adding basic layout for products component

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,12 +1,14 @@
 import React from "react"
 
 const Products = () => (
-    <div>
-        <div>Purses & clutches</div>
-        <div>Bags</div>
-        <div>Card and coin holders</div>
-        <div>Sleeves</div>
-        <div>Christmas stockings</div>
+    <div className="products">
+        <div className="products__container wrapper flex flex--products">
+            <div className="product">Purses & Clutches</div>
+            <div className="product">Bags</div>
+            <div className="product">Card and Coin Holders</div>
+            <div className="product">Sleeves</div>
+            <div className="product">Christmas Stockings</div>
+        </div>
     </div>
 )
 
