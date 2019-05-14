fd7c50db31e712844b9ec83a9f8c04df2b7032bb
T.J <t.ruggles@live.ca>
2019-05-14 16:40:56 -0400

    Styling heroBanner component

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,17 +1,29 @@
 import React from "react"
 
+import Logo from "../images/logo.png"
+
 const HeroBanner = () => (
-    <div>
-        <div>
-            <img src="#" alt="JRCreations logo" />
-            <p>
-                JRCreations is an Ottawa based home business. We can be found at
-                many local craft shows in the fall and spring. Our products are
-                unique and one of a kind. Among the various products listed
-                here, we also can create evening attire, for weddings, proms and
-                other black tie occasions. Take a look around and keep
-                JRCreations in mind for all your crafting ​needs.​​
-            </p>
+    <div className="heroBanner">
+        <div className="heroBanner__overlay">
+            <div className="heroBanner__container">
+                <img
+                    className="heroBanner__logo"
+                    src={Logo}
+                    alt="JRCreations logo"
+                />
+                <p className="heroBanner__text">
+                    JR Creations is an Ottawa based home sewing business. We can
+                    be found at many local craft shows in the fall and spring.
+                    Our products are unique and one of a kind.
+                </p>
+                <p className="heroBanner__text heroBanner__text--cursive">
+                    Take a look around and keep JR Creations in mind for all
+                    your crafting ​needs.
+                </p>
+                <a href="#">
+                    <i className="heroBanner__icon fas fa-chevron-down" />
+                </a>
+            </div>
         </div>
     </div>
 )
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,36 +1,48 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
+import React from "react"
+import Helmet from "react-helmet"
+import { StaticQuery, graphql } from "gatsby"
 
-import 'sanitize.css'
+import "sanitize.css"
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
-        {/*<h1>{data.site.siteMetadata.title}</h1>*/}
-        <div>{children}</div>
-      </>
-    )}
-  />
+    <StaticQuery
+        query={graphql`
+            query SiteTitleQuery {
+                site {
+                    siteMetadata {
+                        title
+                    }
+                }
+            }
+        `}
+        render={(data) => (
+            <>
+                <Helmet
+                    title={data.site.siteMetadata.title}
+                    meta={[
+                        { name: "description", content: "Sample" },
+                        { name: "keywords", content: "sample, something" },
+                    ]}
+                >
+                    <html lang="en" />
+                    <title>JR Creations</title>
+                    <meta charSet="utf-8" />
+                    <link
+                        href="https://fonts.googleapis.com/css?family=Dancing+Script|Roboto:300,400"
+                        rel="stylesheet"
+                    />
+                    <link
+                        rel="stylesheet"
+                        href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+                        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
+                        crossorigin="anonymous"
+                    />
+                </Helmet>
+                {/*<h1>{data.site.siteMetadata.title}</h1>*/}
+                <div>{children}</div>
+            </>
+        )}
+    />
 )
 
 export default Layout
