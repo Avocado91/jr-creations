5e17a0d176a4c8fd875ff4821a6e15a5915f6e4c
T.J <t.ruggles@live.ca>
2019-05-17 21:44:30 -0400

    Finishing products component layout

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,11 +3,15 @@ import React from "react"
 const Products = () => (
     <div className="products">
         <div className="products__container wrapper flex flex--products">
-            <div className="product">Purses & Clutches</div>
-            <div className="product">Bags</div>
-            <div className="product">Card and Coin Holders</div>
-            <div className="product">Sleeves</div>
-            <div className="product">Christmas Stockings</div>
+            <div className="product product--purses">Purses & Clutches</div>
+            <div className="product product--bags">Bags</div>
+            <div className="product product--cardHolders">
+                Card and Coin Holders
+            </div>
+            <div className="product product--sleeves">Sleeves</div>
+            <div className="product product--stockings">
+                Christmas Stockings
+            </div>
         </div>
     </div>
 )
