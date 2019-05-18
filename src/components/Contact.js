d2236dd4d32d8c919e5cc9ef80946335a5b8ab92
T.J <t.ruggles@live.ca>
2019-05-17 22:31:01 -0400

    Styling contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,57 @@
 import React from "react"
 
 const Contact = () => (
-    <div>
-        <h1>Get in Touch</h1>
-        <form>
-            <input type="text" name="name" placeholder="Name" />
-            <input type="text" name="email" placeholder="Email" />
-            <input type="textarea" name="message" placeholder="Message" />
-            <button>Submit</button>
-        </form>
+    <div className="contact">
+        <div className="wrapper">
+            <h1 className="contact__title">Get in Touch</h1>
+            <form
+                method="post"
+                action="#"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+            >
+                <input type="hidden" name="bot-field" />{" "}
+                {/*Netlify anti-spam bot*/}
+                <div className="contact__field-container">
+                    <label className="contact__label" htmlFor="name">
+                        Name
+                    </label>
+                    <input
+                        className="contact__input"
+                        type="text"
+                        name="name"
+                        id="name"
+                    />
+                </div>
+                <div className="contact__field-container">
+                    <label className="contact__label" htmlFor="email">
+                        Email
+                    </label>
+                    <input
+                        className="contact__input"
+                        type="text"
+                        name="email"
+                        id="email"
+                    />
+                </div>
+                <div className="contact__field-container">
+                    <label className="contact__label" htmlFor="message">
+                        Message
+                    </label>
+                    <textarea
+                        className="contact__input"
+                        name="message"
+                        id="message"
+                        rows="6"
+                    />
+                </div>
+                <input
+                    className="contact__button"
+                    type="submit"
+                    value="Send Message"
+                />
+            </form>
+        </div>
     </div>
 )
 
